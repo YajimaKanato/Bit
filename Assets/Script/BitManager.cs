c0e7e755a27dcb1cca51dab3684a2f1a8ec9b4c2
KNTy1018 <soramametaro1018@gmail.com>
2025-09-02 21:12:16 +0900

    [update]遊び方模索中

--- Assets/Script/BitManager.cs
+++ Assets/Script/BitManager.cs
@@ -1,17 +1,25 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class BitManager : MonoBehaviour
 {
-    [SerializeField] GameObject[] _bitText;
-    [SerializeField] GameObject _multiplierText;
+    [SerializeField] GameObject[] _bitTextGroup;
+    [SerializeField] GameObject _multiplierTextGroup;
+    [SerializeField] GameObject _operationButtonGroup;
     [SerializeField] int _range = 7;
 
     Text[,] _bitTexts;
     Text[] _multiplierTexts;
+    Button[] _operationButtons;
 
     int[,] _bitSquare;
-    int[] _bit;
+    int[] _bitTarget;
+    public int[] BitTarget { get { return _bitTarget; } }
+    int _rowIndex;
+    int _columnIndex;
+    bool _rightCross;
+    bool _leftCross;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,18 +27,18 @@ public class BitManager : MonoBehaviour
         SetUp();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
+    /// <summary>
+    /// 初期設定を行う関数
+    /// </summary>
     void SetUp()
     {
         _bitSquare = new int[_range, _range];
-        _bit = new int[_range];
+        _bitTarget = new int[_range];
         _bitTexts = new Text[_range, _range];
         _multiplierTexts = new Text[_range];
+        _operationButtons = new Button[_range];
+        _rowIndex = -1;
+        _columnIndex = -1;
 
         int rand;
         for (int i = 0; i < _range; i++)
@@ -40,26 +48,27 @@ public class BitManager : MonoBehaviour
                 rand = Random.Range(0, 2);
                 _bitSquare[i, j] = rand;
 
-                _bitTexts[i, j] = _bitText[i].transform.GetChild(j).GetComponent<Text>();
+                _bitTexts[i, j] = _bitTextGroup[i].transform.GetChild(j).GetComponent<Text>();
             }
         }
 
         for (int i = 0; i < _range; i++)
         {
-            _multiplierTexts[i] = _multiplierText.transform.GetChild(i).GetComponent<Text>();
+            _multiplierTexts[i] = _multiplierTextGroup.transform.GetChild(i).GetComponent<Text>();
+            _operationButtons[i] = _operationButtonGroup.transform.GetChild(i).GetComponent<Button>();
         }
 
         BitUpdate();
     }
 
     /// <summary>
-    /// 演算後の更新を行う関数
+    /// 演算結果の反映を行う関数
     /// </summary>
     void BitUpdate()
     {
         for (int i = 0; i < _range; i++)
         {
-            _bit[i] = _bitSquare[_range / 2, i];
+            _bitTarget[i] = _bitSquare[_range / 2, i];
             _multiplierTexts[i].text = _bitSquare[_range / 2, i].ToString();
         }
 
@@ -70,119 +79,313 @@ public class BitManager : MonoBehaviour
                 _bitTexts[i, j].text = _bitSquare[i, j].ToString();
             }
         }
+
+        _rowIndex = -1;
+
+        ButtonStateUpdate();
     }
+
+    //ビット演算は右または下を基準に計算する
 
     /// <summary>
     /// AND演算を行う関数
     /// </summary>
-    /// <param name="index">指定した列のインデックス</param>
-    public void ANDOperation(int index)
+    public void ANDOperation()
     {
-        for (int i = 0; i < _range; i++)
+        if (_rowIndex == -1 && _columnIndex == -1) return;
+
+        //行が選択されていたら
+        if (_rowIndex != -1)
         {
-            if (_bitSquare[index, i] * _bit[i] == 1)
+            for (int i = 0; i < _range; i++)
             {
-                _bitSquare[index, i] = 1;
+                if (_bitSquare[_rowIndex, i] * _bitTarget[i] == 1)
+                {
+                    _bitSquare[_rowIndex, i] = 1;
+                }
+                else
+                {
+                    _bitSquare[_rowIndex, i] = 0;
+                }
             }
-            else
+        }
+
+        //列が選択されていたら
+        if (_columnIndex != -1)
+        {
+            for (int i = 0; i < _range; i++)
             {
-                _bitSquare[index, i] = 0;
+                if (_bitSquare[i, _columnIndex] * _bitTarget[i] == 1)
+                {
+                    _bitSquare[i, _columnIndex] = 1;
+                }
+                else
+                {
+                    _bitSquare[i, _columnIndex] = 0;
+                }
             }
         }
 
         BitUpdate();
+        OneCheck();
     }
 
     /// <summary>
     /// OR演算を行う関数
     /// </summary>
-    /// <param name="index">指定した列のインデックス</param>
-    public void OROperation(int index)
+    public void OROperation()
     {
-        for (int i = 0; i < _range; i++)
+        if (_rowIndex == -1 && _columnIndex == -1) return;
+
+        if (_rowIndex != -1)
         {
-            if (_bitSquare[index, i] == 1 || _bit[i] == 1)
+            for (int i = 0; i < _range; i++)
             {
-                _bitSquare[index, i] = 1;
+                if (_bitSquare[_rowIndex, i] == 1 || _bitTarget[i] == 1)
+                {
+                    _bitSquare[_rowIndex, i] = 1;
+                }
+                else
+                {
+                    _bitSquare[_rowIndex, i] = 0;
+                }
             }
-            else
+        }
+
+        if (_columnIndex != -1)
+        {
+
+            for (int i = 0; i < _range; i++)
             {
-                _bitSquare[index, i] = 0;
+                if (_bitSquare[i, _columnIndex] == 1 || _bitTarget[i] == 1)
+                {
+                    _bitSquare[i, _columnIndex] = 1;
+                }
+                else
+                {
+                    _bitSquare[i, _columnIndex] = 0;
+                }
             }
         }
 
         BitUpdate();
+        OneCheck();
     }
 
     /// <summary>
     /// XOR演算を行う関数
     /// </summary>
-    /// <param name="index">指定した列のインデックス</param>
-    public void XOROperation(int index)
+    public void XOROperation()
     {
-        for (int i = 0; i < _range; i++)
+        if (_rowIndex == -1 && _columnIndex == -1) return;
+
+        if (_rowIndex != -1)
         {
-            if (_bitSquare[index, i] == _bit[i])
+            for (int i = 0; i < _range; i++)
             {
-                _bitSquare[index, i] = 0;
+                if (_bitSquare[_rowIndex, i] == _bitTarget[i])
+                {
+                    _bitSquare[_rowIndex, i] = 0;
+                }
+                else
+                {
+                    _bitSquare[_rowIndex, i] = 1;
+                }
             }
-            else
+        }
+
+        if (_columnIndex != -1)
+        {
+            for (int i = 0; i < _range; i++)
             {
-                _bitSquare[index, i] = 1;
+                if (_bitSquare[i, _columnIndex] == _bitTarget[i])
+                {
+                    _bitSquare[i, _columnIndex] = 0;
+                }
+                else
+                {
+                    _bitSquare[i, _columnIndex] = 1;
+                }
             }
         }
 
         BitUpdate();
+        OneCheck();
     }
 
     /// <summary>
     /// NOT演算を行う関数
     /// </summary>
-    /// <param name="index">指定した列のインデックス</param>
-    public void NOTOperation(int index)
+    public void NOTOperation()
     {
-        for (int i = 0; i < _range; i++)
+        if (_rowIndex == -1 && _columnIndex == -1) return;
+
+        if (_rowIndex != -1)
         {
-            if (_bitSquare[index, i] == 1)
+            for (int i = 0; i < _range; i++)
             {
-                _bitSquare[index, i] = 0;
+                if (_bitSquare[_rowIndex, i] == 1)
+                {
+                    _bitSquare[_rowIndex, i] = 0;
+                }
+                else
+                {
+                    _bitSquare[_rowIndex, i] = 1;
+                }
             }
-            else
+        }
+
+        if (_columnIndex != -1)
+        {
+            for (int i = 0; i < _range; i++)
             {
-                _bitSquare[index, i] = 1;
+                if (_bitSquare[i, _columnIndex] == 1)
+                {
+                    _bitSquare[i, _columnIndex] = 0;
+                }
+                else
+                {
+                    _bitSquare[i, _columnIndex] = 1;
+                }
             }
         }
 
         BitUpdate();
+        OneCheck();
     }
 
     /// <summary>
     /// 左ビットシフトを行う関数
     /// </summary>
-    /// <param name="index">指定した行のインデックス</param>
-    public void LeftShift(int index)
+    public void LeftorUpShift()
     {
-        for (int i = 0; i < _range - 1; i++)
+        if (_rowIndex == -1 && _columnIndex == -1) return;
+
+        if (_rowIndex != -1)
         {
-            _bitSquare[index, i] = _bitSquare[index, i + 1];
+            for (int i = 0; i < _range - 1; i++)
+            {
+                _bitSquare[_rowIndex, i] = _bitSquare[_rowIndex, i + 1];
+            }
+            _bitSquare[_rowIndex, _range - 1] = 0;
         }
-        _bitSquare[index, _bitSquare.GetLength(0) - 1] = 0;
+
+        if (_columnIndex != -1)
+        {
+            for (int i = 0; i < _range - 1; i++)
+            {
+                _bitSquare[i, _columnIndex] = _bitSquare[i + 1, _columnIndex];
+            }
+            _bitSquare[_range - 1, _columnIndex] = 0;
+        }
 
         BitUpdate();
+        OneCheck();
     }
 
     /// <summary>
     /// 右ビットシフトを行う関数
     /// </summary>
-    /// <param name="index">指定した行のインデックス</param>
-    public void RightShift(int index)
+    public void RightorDownShift()
     {
-        for (int i = 1; i < _range; i++)
+        if (_rowIndex == -1 && _columnIndex == -1) return;
+
+        if (_rowIndex != -1)
         {
-            _bitSquare[index, _range - i] = _bitSquare[index, _range - i - 1];
+            for (int i = 1; i < _range; i++)
+            {
+                _bitSquare[_rowIndex, _range - i] = _bitSquare[_rowIndex, _range - 1 - i];
+            }
+            _bitSquare[_rowIndex, 0] = 0;
         }
-        _bitSquare[index, 0] = 0;
+
+        if (_columnIndex != -1)
+        {
+            for (int i = 1; i < _range; i++)
+            {
+                _bitSquare[_range - i, _columnIndex] = _bitSquare[_range - 1 - i, _columnIndex];
+            }
+            _bitSquare[0, _columnIndex] = 0;
+        }
 
         BitUpdate();
+        OneCheck();
+    }
+
+    public void SelectedRowIndex(int index)
+    {
+        _rowIndex = index;
+        _columnIndex = -1;
+        Debug.Log("Row : " + _rowIndex);
+    }
+
+    public void SelectedColumnIndex(int index)
+    {
+        _columnIndex = index;
+        _rowIndex = -1;
+        Debug.Log("Column : " + _columnIndex);
+    }
+
+    /// <summary>
+    /// 斜めに１がそろっているかを調べる関数
+    /// </summary>
+    void OneCheck()
+    {
+        _rightCross = true;
+        _leftCross = true;
+
+        //右肩下がりを調べる
+        for (int i = 0; i < _range; i++)
+        {
+            if (_bitSquare[i, i] != 1)
+            {
+                _rightCross = false;
+                break;
+            }
+        }
+
+        //右肩上がりを調べる
+        for (int i = 0; i < _range; i++)
+        {
+            if (_bitSquare[i, _range - 1 - i] != 1)
+            {
+                _leftCross = false;
+                break;
+            }
+        }
+
+        for (int i = 0; i < _range; i++)
+        {
+            if (_rightCross)
+            {
+                _bitSquare[i, i] = 0;
+                _bitTexts[i, i].text = _bitSquare[i, i].ToString();
+
+            }
+
+            if (_leftCross)
+            {
+                _bitSquare[i, _range - 1 - i] = 0;
+                _bitTexts[i, _range - 1 - i].text = _bitSquare[i, _range - 1 - i].ToString();
+            }
+        }
+    }
+
+    /// <summary>
+    /// ビットの値によってボタンの使用可能状態を切り替える関数
+    /// </summary>
+    void ButtonStateUpdate()
+    {
+        for (int i = 0; i < _range; i++)
+        {
+            if (_bitTarget[i] == 1)
+            {
+                _operationButtons[i].interactable = true;
+            }
+            else
+            {
+                _operationButtons[i].interactable = false;
+            }
+        }
     }
 }
